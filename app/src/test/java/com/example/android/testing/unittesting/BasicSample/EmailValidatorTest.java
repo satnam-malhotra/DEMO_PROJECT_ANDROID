7358d82fc5c38f5942fdcdc5d833514ed3ec6927
Satnam Malhotra <satnam.malhotra@3pillarglobal.com>
2019-07-30 17:40:28 +0530

    Made few changes in jenkins file

--- app/src/test/java/com/example/android/testing/unittesting/BasicSample/EmailValidatorTest.java
+++ app/src/test/java/com/example/android/testing/unittesting/BasicSample/EmailValidatorTest.java
@@ -30,7 +30,7 @@ public class EmailValidatorTest {
 
     @Test
     public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
-        assertTrue(EmailValidator.isValidEmail("naemail.com"));
+        assertTrue(EmailValidator.isValidEmail("name@email.com"));
     }
 
     @Test
